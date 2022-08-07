91bc4fea35e67de9f79bdb46c6a3a6470878abc2
Saumy0010 <s8795p@gmail.com>
2022-08-07 12:41:05 +0530

    Add login page

--- app.js
+++ app.js
@@ -21,10 +21,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-
 
 app.post('/', (req, res) => {
     const regUser = async() => {
@@ -40,6 +36,30 @@ app.post('/', (req, res) => {
     }
     regUser()
 })
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html')
+})
+app.post('/login', (req, res) => {
+    const loginUser = async() => {
+        try {
+            const user = await Reg.findOne({ username: req.body.username })
+            if (!user) {
+                res.send('User not found')
+            } else {
+                if (user.password === req.body.password) {
+                    res.send('User logged in successfully')
+                } else {
+                    res.send('Incorrect password')
+                }
+            }
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+    loginUser()
+})
+
 
 app.listen(port, () => {
     console.log(`Login app listening on port http://localhost:${port}`)
